40ebc1021ddf34172bec24c1cdfd6a6790cd1646
JABNIA <vaxojabnia@gmail.com>
2025-11-11 20:24:44 +0400

    table created

--- webapp/controller/Main.controller.js
+++ webapp/controller/Main.controller.js
@@ -1,10 +1,16 @@
 sap.ui.define([
-    "sap/ui/core/mvc/Controller"
-], (Controller) => {
+    "sap/ui/core/mvc/Controller",
+    "sap/ui/model/json/JSONModel"
+], (Controller, JSONModel) => {
     "use strict";
 
     return Controller.extend("project1.controller.Main", {
         onInit() {
+            const oBookModel = new JSONModel();
+
+            oBookModel.loadData("model/books.json");
+
+            this.getView().setModel(oBookModel, "bookData");
         }
     });
 });
